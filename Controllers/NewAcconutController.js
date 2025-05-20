9708082691659d6d686a8dbcd2970d948eb08f3d
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-05-20 20:08:39 -0300

    Update NewAcconutController.js
    iniciando a logica de nogocio que gerencia novas contas para usar o site

--- Controllers/NewAcconutController.js
+++ Controllers/NewAcconutController.js
@@ -1,6 +1,25 @@
 module.exports = {
     index: function(req, res)
     {
-        res.render('views/account/index');
+        res.render('views/account/index', {pathName:'main-account'});
+    },
+    verifyEmail:function(email, req, res)
+    {
+        //funcao para verficar se existe o email na base de dados
+        //se nao existir redireciona para cadastro de nome
+        //e salva na sessao o email
+        this.name(req, res)
+
+        //se existir redireciona para tela de login
+        //com mesnagem dizendo que o email ja existe e que pode fazer a recuperação de senha
+    },
+    name:function(req, res){
+         res.render('views/account/index',{pathName:"main-new-account-name"});
+    },
+    saveName: function(name, res)
+    {
+        //salva na sessao o nome da pessoa e redireciona pra a pagina de seleção de serviços
+        res.render('views/account/index', {pathName:"main-new-account-options-service"});
     }
+
 }

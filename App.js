d01b8939cc259b267318351c25a267788e43a8b4
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-05-28 15:59:29 -0300

    Update App.js
    incluido as configuração de sessao e autenticacao, por enquanto nao ativo

--- App.js
+++ App.js
@@ -3,6 +3,14 @@ const App = express();
 const port = 8080;
 const hostName = 'localhost'
 const router = require('./Router/Router');
+
+/*configs para reativar no futuro 
+quando estiver usando os modolus de sessao, autenticacao
+verificar a configuração de rotas
+const session = require('express-session');
+const passport = require('passport');
+const router = require('./routers/router');
+const authRouter = require('./routers/authRouter');*/
 /*--- App CONFIGS ---*/
 
 //App use's
@@ -10,6 +18,22 @@ App.use(express.static("public"));
 App.use(express.json());
 App.use(express.urlencoded({extended:true}));
 
+/*configs para reativar no futuro 
+quando estiver usando os modolus de sessao, autenticacao
+app.use(session({
+    secret:'segredo',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        secure: false,
+        httpOnly: true,
+        maxAge: 600000
+    }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(router);
+app.use(authRouter);*/
 
 //App set's
 App.set("views", __dirname, "views");

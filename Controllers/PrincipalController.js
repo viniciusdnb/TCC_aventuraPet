42cfd7011d1d08094a4eb340d4878ed3bf45c86a
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-06-05 14:59:56 -0300

    Update PrincipalController.js

--- Controllers/PrincipalController.js
+++ Controllers/PrincipalController.js
@@ -1,6 +1,10 @@
 module.exports = {
     index: function(req, res)
     {
-        res.render('views/principal/index', {pathName:"principal-main"});
+        //ver se o user ja tem pete cadastrado
+        //caso nao tenha mandar imagem que indica que ainda nao tem
+        const imgPet = false
+        
+        res.render('views/principal/index', {pathName:"principal-main", imgPet:imgPet});
     }
 }

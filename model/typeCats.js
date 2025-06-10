54b713d30ed6b66d40add9406c2ffd1b6e349ea7
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-06-10 16:23:54 -0300

    arquivos teste de dados

---
+++ model/typeCats.js
@@ -0,0 +1,10 @@
+module.exports = {
+    getRacaPets:[
+        {key:1, name:"Abssinio", endImg:"/img/abissinio.webp", type:"cat"},
+        {key:2, name:"American Shorthair", endImg: "/img/american-shorthair.webp", type:"cat"},
+        {key:3, name:"Ashera", endImg: "/img/ashera.webp", type:"cat"},
+        {key:4, name:"Balines", endImg:"/img/balines.webp", type:"cat"},
+        {key:5, name:"Bengal", endImg:"/img/bengal.webp", type:"cat"}   
+
+    ]
+}
---
+++ model/typeDogs.js
@@ -0,0 +1,9 @@
+module.exports = {
+    getRacaPets:[
+        {key:1, name: "affenpinscher", endImg:"/img/affenpinscher.webp", type:"dog"},
+        {key:2, name: "aidi", endImg:"/img/aidi.webp", type:"dog"},
+        {key:3, name: "airedale-terrier", endImg:"/img/airedale-terrier.webp", type:"dog"},
+        {key:4, name: "akita-americano", endImg:"/img/akita-americano.webp", type:"dog"},
+        {key:5, name: "alusky-malamute", endImg:"/img/alusky-malamute.webp", type:"dog"}
+    ]
+}

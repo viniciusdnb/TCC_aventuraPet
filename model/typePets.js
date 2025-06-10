38351cca5aed430315a5b85db1b43525d1e07064
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-06-10 11:13:32 -0300

    Create typePets.js

---
+++ model/typePets.js
@@ -0,0 +1,11 @@
+module.exports = {
+    getTypePets:[
+        { key:1, value:"Gatos"},
+        { key:2, value:"Cachorros"},
+        { key:3, value:"Aves"},
+        { key:4, value:"Roedores"},
+        { key:5, value:"Repteis"},
+        { key:6, value:"Insetos"}
+
+    ]
+}
